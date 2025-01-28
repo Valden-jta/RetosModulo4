25542497567eacc2e810ae4314463c999ecb5fb2
Valden-jta <javierdlt45@gmail.com>
2025-01-28 14:26:28 +0100

    ejercicios 2 y 3 completados

---
+++ dia 3/reto2-3/src/models/bookI.js
@@ -0,0 +1,12 @@
+class Book {
+
+    constructor(id_book, title, author, genre) {
+        this.id_book = id_book;
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+    }
+
+}
+
+module.exports = { Book };
